7e78b3d80344952c928bb0b79f2d111b0a003f44
zizighadban <zahra.ghadban@se.experis.com>
2022-02-21 16:12:02 +0100

    FEAT: Connected mapping to database request

--- src/main/java/com/main/assignmentsix/controllers/CustomerController.java
+++ src/main/java/com/main/assignmentsix/controllers/CustomerController.java
@@ -1,16 +1,24 @@
 package com.main.assignmentsix.controllers;
 
 import com.main.assignmentsix.models.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.main.assignmentsix.repositories.CustomerRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 
 @RestController
 public class CustomerController {
 
-    /*
-    @GetMapping("/customers/all")
-    public Customer[] allCustomers(){
-        //Customer x = new Customer();
+    private CustomerRepository customerRepository = new CustomerRepository();
+
+
+    @RequestMapping(value="/api/customers/{id}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable String id){
+        return customerRepository.getCustomerById(id);
     }
-    */
+
 }
