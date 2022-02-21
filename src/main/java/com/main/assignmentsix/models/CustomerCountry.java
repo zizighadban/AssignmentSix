617176d5fa915f829e09c52fc3f7c8bc3d7befbe
zizighadban <zahra.ghadban@se.experis.com>
2022-02-21 16:07:28 +0100

    CHORE: Added states, constructor and getters for two models

--- src/main/java/com/main/assignmentsix/models/CustomerCountry.java
+++ src/main/java/com/main/assignmentsix/models/CustomerCountry.java
@@ -1,4 +1,19 @@
 package com.main.assignmentsix.models;
 
 public class CustomerCountry {
+    private int amountOfCustomers;
+    private String country;
+
+    public CustomerCountry(int amountOfCustomers, String country) {
+        this.amountOfCustomers = amountOfCustomers;
+        this.country = country;
+    }
+
+    public int getAmountOfCustomers() {
+        return amountOfCustomers;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
--- src/main/java/com/main/assignmentsix/models/CustomerSpender.java
+++ src/main/java/com/main/assignmentsix/models/CustomerSpender.java
@@ -1,4 +1,19 @@
 package com.main.assignmentsix.models;
 
 public class CustomerSpender {
+    private int customerId;
+    private double invoiceTotal;
+
+    public CustomerSpender(int customerId, double invoiceTotal) {
+        this.customerId = customerId;
+        this.invoiceTotal = invoiceTotal;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public double getInvoiceTotal() {
+        return invoiceTotal;
+    }
 }
