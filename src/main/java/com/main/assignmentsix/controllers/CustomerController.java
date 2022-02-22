89df0e3c5ba62e608c45adb45db4d8dae5849cb2
ChristianNeij <christianneij@gmail.com>
2022-02-22 14:51:47 +0100

    feat: Created end-point for getting all customers from api.

--- src/main/java/com/main/assignmentsix/controllers/CustomerController.java
+++ src/main/java/com/main/assignmentsix/controllers/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("api")
@@ -21,9 +23,12 @@ public class CustomerController {
     }
 
 
-    @RequestMapping(value="customers/{id}", method = RequestMethod.GET)
+    @RequestMapping(value="customer/{id}", method = RequestMethod.GET)
     public Customer getCustomerById(@PathVariable String id){
         return customerRepository.getCustomerById(id);
     }
+
+    @RequestMapping(value="customer", method = RequestMethod.GET)
+    public List<Customer> getAllCustomers(){return customerRepository.getAllCustomers();}
 
 }
