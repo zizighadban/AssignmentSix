9ea06e099414e11f39c3666dababe24bdb6884a9
ChristianNeij <christianneij@gmail.com>
2022-02-22 14:25:08 +0100

    feat: Created Repository interface with relevant methods declared.

---
+++ src/main/java/com/main/assignmentsix/data_access/ICustomerRepository.java
@@ -0,0 +1,22 @@
+package com.main.assignmentsix.data_access;
+
+import com.main.assignmentsix.models.Customer;
+import com.main.assignmentsix.models.CustomerCountry;
+import com.main.assignmentsix.models.CustomerGenre;
+import com.main.assignmentsix.models.CustomerSpender;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ICustomerRepository {
+
+    public Customer getCustomerById(String customerId);
+    public List<Customer> getAllCustomers();
+    public Customer getCustomerByFirstName(String firstName);
+    public List<Customer> getSpecificAmountOfCustomer(int limit, int offset);
+    public boolean addCustomer(Customer newCustomer);
+    public List<CustomerCountry> getCustomerCountry();
+    public List<CustomerSpender> getCustomerSpender();
+    public CustomerGenre getCustomerGenre(int customerId);
+}
