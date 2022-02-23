bafed3a776c5fcd8da5ac94ad52394ff5a1a588e
zizighadban <zahra.ghadban@se.experis.com>
2022-02-23 16:58:14 +0100

    CHORE: Removed old GetMapping for start-page

--- src/main/java/com/main/assignmentsix/AssignmentSixApplication.java
+++ src/main/java/com/main/assignmentsix/AssignmentSixApplication.java
@@ -2,27 +2,14 @@ package com.main.assignmentsix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class AssignmentSixApplication {
 
-    public class Message{
-        public String message;
-
-        public Message (String message){
-            this.message = message;
-        }
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(AssignmentSixApplication.class, args);
     }
 
-    @GetMapping("/")
-    public Message hello(){
-        return new Message("Hello, World!");
-    }
 }
