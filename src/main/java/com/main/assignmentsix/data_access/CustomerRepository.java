64d1a4e95cf5ad72c4e21b69f99edd5ebf662da3
ChristianNeij <christianneij@gmail.com>
2022-02-24 08:54:34 +0100

    fix: Searching for track in thymeleaf caused heap memory error, SQL-fetch was badly written causing an overload of information. Now works properly.

--- src/main/java/com/main/assignmentsix/data_access/CustomerRepository.java
+++ src/main/java/com/main/assignmentsix/data_access/CustomerRepository.java
@@ -503,19 +503,17 @@ public class CustomerRepository implements ICustomerRepository{
 
             // Make SQL query
             PreparedStatement preparedStatement =
-                    conn.prepareStatement("SELECT Genre.Name AS GenreName, Track.Name AS TrackName, Album.Title AS AlbumName, Artist.Name AS ArtistName FROM Genre, Track, Album, Artist WHERE TrackName LIKE ?");
+                    conn.prepareStatement("SELECT Genre.Name AS GenreName, Track.Name AS TrackName, Album.Title AS AlbumName, Artist.Name AS ArtistName FROM Genre, Track, Album, Artist WHERE Genre.GenreId = Track.GenreId AND Album.AlbumId = Track.AlbumId AND Artist.ArtistId = Album.ArtistId AND TrackName LIKE ?");
             preparedStatement.setString(1,"%"+song.getSongName()+"%");
-
             // Execute Query
             ResultSet resultSet = preparedStatement.executeQuery();
-
             while (resultSet.next()) {
-               trackList.add(new Track(
+                trackList.add(new Track(
                        new Artist(resultSet.getString("ArtistName")),
                        resultSet.getString("AlbumName"),
                        new Song(resultSet.getString("TrackName")),
                        new Genre(resultSet.getString("GenreName"))
-               ));
+                ));
             }
             System.out.println("Select track successful");
         }
